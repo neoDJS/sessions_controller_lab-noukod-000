a23dc38c72e86667f37a2844d91917ff8dfa166f
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-03-29 17:01:13 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if params[:name]
     session[:username] = params[:name]
 
     redirect_to root_path
+  else
+    redirect_to login_path
   end
 
   def destroy
